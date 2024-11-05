3f5733fe92ec583d03f002c155f461c211c3f565
roman9234 <roman_lesovoy@mail.ru>
2024-11-05 15:28:33 +0300

    started booking project

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for booking project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -17,10 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-# urlpatterns = [
-#     path('posts/', PostList.as_view(), name='post-list'),
-# ]
